cfcb458185dfb0af8b7372924f053ba517f77f8a
しぐま <hoshizora.working@gmail.com>
2025-12-28 13:48:22 +0900

    index.php を更新

--- kakeibo-app/index.php
+++ kakeibo-app/index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <meta charset="uft-8">
+        <meta charset="uft-8" />
+        <link href="index.css" type="text/stylesheet" />
         <title>家計簿app</title>
     </head>
+    <body>
+        
+    </body>
 </html>
