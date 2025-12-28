c9218979631ec410403202b98380f457dc8b3e6c
しぐま <hoshizora.working@gmail.com>
2025-12-28 12:22:07 +0000

    new file:   kakeibo-app/non_user.php 	deleted:    kakeibo-app/index.php

---
+++ kakeibo-app/non_user.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="uft-8" />
+        <link href="index.css" type="text/stylesheet" />
+        <title>家計簿app</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="/">Home</a>
+            </nav>
+        </header>
+        <main>
+            <h1>家計簿アプリ</h1>
+        </main>
+        <footer>
+            <p>Copyright &copy; 2025 signa</p>
+        </footer>
+    </body>
+</html>
